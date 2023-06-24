2c2fccffd09f8f72cea0d98a063ecfc718b83be5
ahmadillo20007 <ahmadilloqubijonov74@gmail.com>
2023-06-24 16:42:10 +0500

    zh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+
+      </Routes>
     </div>
   );
 }
